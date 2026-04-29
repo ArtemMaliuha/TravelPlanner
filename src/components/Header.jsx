e891afe516557762a64feb261d4b129fe2dba5d8
Artem Maliuha <malugaartem072@gmail.com>
2026-04-29 10:58:59 +0300

    feat: add firebase auth and login/logout logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import { VscCompass, VscSearch } from "react-icons/vsc"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { auth, googleProvider } from "../firebase.js"
+import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth"
 
 export default function Header() {
 
     const [isOpen, setIsOpen] = useState(false)  
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log("Поточний користувач (onAuthStateChanged):", currentUser)
+            setUser(currentUser)
+        })
+        return () => unsubscribe()
+    },[])
+
+    const handleLogin = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+        } catch {
+            console.error(error.message)
+        }
+    }
+
+    const handleLogout = () => signOut(auth)
+
+
 
     return (
         <div className="flex w-full border-b-[2px] border-gray-200 h-[56px] items-center justify-between">
@@ -15,12 +38,23 @@ export default function Header() {
                 <h2 className="text-[20px] pt-[3px] ml-[24px] font-medium">My Trips</h2>
                 <h2 className="text-[20px] pt-[3px] ml-[24px] font-medium">Something else</h2>
             </div>
-            <div className="w-auto flex items-center mr-[48px]">
+            <div className="w-auto flex items-center mr-4">
                 <div className="relative flex items-center mr-3">
                     <button onClick={() => setIsOpen(isOpen => !isOpen)} className="absolute left-1"><VscSearch /></button>
                     <input type="text" name="search" className={`h-7 text-[18px] border-gray-200 border-[2px] rounded-lg transition-all duration-500 ease-in-out ${isOpen ? "w-64 opacity-100 p-2 pl-6" : "w-0 opacity-0"}` }/>
                 </div>
-                <p className="ml-[8px]">google account placeholder</p>
+                <div className="ml-2 flex">
+                    {
+                        user ? (
+                            <>
+                                <img className="w-8 h-8 rounded-full mx-2" src={user.photoURL} />
+                                <button onClick={() => handleLogout()} className="hover:bg-red-400 px-2 border-none rounded-lg">Log out</button>
+                            </>
+                        ) : (
+                            <button className="bg-blue-300 px-2 py-1 border-none rounded-lg ml-2" onClick={() => handleLogin()}>Login</button>
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
